3ae057c55570e7f2a33204a40a1f463f0e88b943
shxblx <shiblibasheer27@gmail.com>
2024-06-11 17:33:13 +0530

    front end done

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -11,9 +11,29 @@ function App() {
       <div className="w=[350px]">
         <form className="mb-5">
           <input className="w-full p-2 rounded-sm mb-2" type="text" />
-          <button type="submit" className="bg-gray-700 w-full p-2">Add</button>
+          <button type="submit" className="bg-gray-700 w-full p-2">
+            Add
+          </button>
         </form>
-        <div></div>
+        <div className="h-52 overflow-y-auto">
+          <div className="flex justify-between items-center border border-slate-600 pl-2 mb-2 mr-1 py-2">
+            <p>JS</p>
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+              >
+                <path
+                  d="M3 6h18v2H3zM5 8h14v14H5V8zm4 2v10h2V10H9zm4 0v10h2V10h-2z"
+                  fill="currentColor"
+                />
+                <path d="M8 4h8v2H8z" fill="currentColor" />
+              </svg>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
